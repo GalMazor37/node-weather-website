16c07c5f8150ce584ca5b81b33cd6e19379c5fc1
GalMazor37 <galmazor37@gmail.com>
2019-12-02 16:31:22 +0200

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log('Client side')
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+const fetchFunc = (addressCity) => {
+    const myURL = 'http://localhost:3000/weather?address=' + addressCity
+    fetch(myURL).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+}
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetchFunc(location)
+})
---
+++ src/app.js
@@ -0,0 +1,100 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Glen Meiz'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Glen Meiz'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        helpText: 'My challenge',
+        name: 'Glen Meiz',
+        title: 'Help'
+    })
+})
+
+
+app.get('/weather', (req,res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error){
+            return res.send({ error }) // equal to error: error
+        }
+        forecast(latitude, longitude, (error, forecastData) =>{
+            if (error){
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+
+            })
+        })
+    })
+})
+
+app.get('/products', (req,res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products:[]
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'Help article not found',
+        name: 'Glen Meiz',
+        title: '404 page'
+    })
+})
+
+app.get('/*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'My 404 page',
+        name: 'Glen Meiz',
+        title: '404 page'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
